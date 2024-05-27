a7588e551d39056fb8bf1db0b1fea7ac4ab993c0
ayyashfaiz <ayyashfaiz957@gmail.com>
2024-05-27 11:25:01 +0530

    my commit

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+        public static void main (String[]args){
+            System.out.println("Book_Name");
+            System.out.println("Book_Author");
+            System.out.println("Book_ID");
+
+        }
+    }
+
+
---
+++ src/LibrarySystem.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class LibrarySystem {
+    public static void main (String[]args){
+        System.out.println("Library_ID");
+        System.out.println("Library_Name");
+        System.out.println("Library_Admin");
+
+    }
+}
